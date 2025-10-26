25f2b35be2fe0bfe0d044fc23e60ab3936eaa525
Akshay Tilwanker <akshaytilwanker@gmail.com>
2025-10-27 04:33:11 +0530

    fix: production build now working

---
+++ next.config.js
@@ -0,0 +1,9 @@
+﻿/** @type {import('next").NextConfig} */
+const nextConfig = {
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+};
+
+module.exports = nextConfig;
